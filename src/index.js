6b824767df1b7781cba1e1d57e41471b2489b847
Alisa Makarova <makaralice97@gmail.com>
2021-01-05 11:27:27 +0200

    added App and SearchBox components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 import { robots } from './robots.js';
 
 ReactDOM.render(
-	<CardList robots={robots}/>,
+	<App/>,
   document.getElementById('root')
 );
 
