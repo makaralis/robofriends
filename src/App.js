439c14c99145c2849f40c60704e6293336476173
Alisa Makarova <makaralice97@gmail.com>
2021-01-06 19:16:06 +0200

    taking the robots data from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
@@ -9,9 +8,15 @@ class App extends Component {
 		// in order to use this we call super
 		super(); 
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users }));
 	}
 
 	onSearchChange = (event) => {
@@ -23,13 +28,19 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return (
+
+		if (this.state.robots.length === 0) {
+			return <h1 className="f1 tc br3 pa3 ma2">Loading...</h1>
+		}
+		else {
+			return (
 			<div className="tc">
 				<h1 className="f1">RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<CardList robots={filteredRobots}/>
 			</div>
 		);
+		}
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
-import { robots } from './robots.js';
 
 ReactDOM.render(
 	<App/>,
