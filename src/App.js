c8d655152f66bfe11388d7a1fe8145d369fb8bc4
Alisa Makarova <makaralice97@gmail.com>
2021-01-06 12:24:29 +0200

    added state to the App and a function to search robots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-	return (
-		<div className="tc">
-			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList robots={robots}/>
-		</div>
-	);
+class App extends Component {
+	constructor() {
+		// in order to use this we call super
+		super(); 
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		//in order to change state 
+		this.setState({ searchfield: event.target.value });
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
+			<div className="tc">
+				<h1>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList robots={filteredRobots}/>
+			</div>
+		);
+	}
 }
 
 export default App;
